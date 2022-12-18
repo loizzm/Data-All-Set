d6132a211181f4c2d90335fef44247e974b846bd
loizzm <ireliontt@gmail.com>
2022-12-17 21:36:39 -0300

    Correções

--- Iot/Data.py
+++ Iot/Data.py
@@ -5,29 +5,29 @@ class Data:
         self._mes=mes
         
     def print_data(self):
-        if (self.get_dia()<10 and self.get_mes()<10):
-            print("0{}".format(self.get_dia()),"0{}".format(self.get_mes()),self.get_ano(),sep="/")
-        elif(self.get_dia()>=10 and self.get_mes()<10):
-            print(self.get_dia(),"0{}".format(self.get_mes()),self.get_ano(),sep="/")
+        if (self.dia<10 and self.mes<10):
+            print("0{}".format(self.dia),"0{}".format(self.mes),self.ano,sep="/")
+        elif(self.dia>=10 and self.mes<10):
+            print(self.dia,"0{}".format(self.mes),self.ano,sep="/")
         elif (self.get_dia()<10 and self.get_mes()>=10):
-            print("0{}".format(self.get_dia()),self.get_mes(),self.get_ano(),sep="/")
+            print("0{}".format(self.dia),self.mes,self.ano,sep="/")
         else:
-            print(self.get_dia(),self.get_mes(),self.get_ano(),sep="/")                 
+            print(self.dia,self.mes,self.ano,sep="/")                 
 
-    def fix_mes(self,other, mod):
+    def __fix_mes(self,other, mod):
         self._mes+=mod
-        if(self._mes < other.get_mes()):
-            mes_o=abs(self._mes-other.get_mes())
+        if(self._mes < other.mes):
+            mes_o=abs(self._mes-other.mes)
             self._mes=12-mes_o
-            self._ano-= other.get_ano()+1
+            self._ano-= other.ano+1
         else:
-            self._mes-=other.get_mes()
-            self._ano-= other.get_ano()
+            self._mes-=other.mes
+            self._ano-= other.ano
 
-    def fix_dia(self,other):
-        if(self._dia < other.get_dia()):
+    def __fix_dia(self,other):
+        if(self._dia < other.dia):
             self.fix_mes(other,-1)
-            dia_o=abs(self._dia-other.get_dia())
+            dia_o=abs(self._dia-other.dia)
             if(self._mes==2 or self._mes==4 or self._mes==6 or self._mes==8 or self._mes==9 or self._mes==11 or self._mes==1 ):
                 self._dia=31-dia_o
             elif(self._mes==3):
@@ -35,12 +35,12 @@ class Data:
             else:
                 self._dia=30-dia_o
         else:
-            self._dia-=other.get_dia() 
-            self.fix_mes(other,0)  
+            self._dia-=other.dia 
+            self.__fix_mes(other,0)  
     
     def __sub__(self,other):
-        if(self.check_op(other) == True):
-            self.fix_dia(other)
+        if(self.__check_op(other) == True):
+            self.__fix_dia(other)
             if(self._ano > 1):
                 print ("A diferença é de {} anos".format(self._ano),end=", ")
             else: 
@@ -57,22 +57,31 @@ class Data:
         else:
             print("****Operação inválida*****") 
             exit  
-    
-    def get_dia(self):
+    @property
+    def dia(self):
         return self._dia
-
-    def get_ano(self):
+    @property
+    def ano(self):
         return self._ano
-    
-    def get_mes(self):
+    @property
+    def mes(self):
         return self._mes
+    @dia.setter
+    def dia(self,dia):
+        self._dia=dia  
+    @mes.setter
+    def mes(self,mes):
+        self._mes=mes 
+    @ano.setter
+    def ano(self,ano):
+        self._ano=ano
 
-    def check_op(self,other):
-        if(self._ano < other.get_ano()):
+    def __check_op(self,other):
+        if(self._ano < other.ano):
             return False
-        elif(self._ano == other.get_ano() and self._mes < other.get_mes()):
+        elif(self._ano == other.ano and self._mes < other.mes):
             return False
-        elif (self._ano == other.get_ano() and self._mes == other.get_mes() and self._dia < other.get_dia()):
+        elif (self._ano == other.ano and self._mes == other.mes and self._dia < other.dia):
             return False
         else:
             return True  
